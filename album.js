207ff457d3089be8cca623d8a58c589e7d144cc6
Ikadev95 <fegughi@gmail.com>
2024-10-01 15:30:37 +0200

    data album ok

--- album.js
+++ album.js
@@ -12,8 +12,22 @@ const getInfoAlbum = function (id) {
       .then((album) => {
         console.log("info album", album);
         let songArray = album.tracks.data;
-        console.log(songArray[1]);
+        console.log(songArray);
+        let title = document.getElementById('title');
+        title.innerText = album.title;
         let table = document.querySelector('table');
+        let albumImage = document.getElementById('album-image');
+        albumImage.src = album.cover_medium;
+        let iconImage = document.getElementById('iconImage');
+        iconImage.src = album.cover_small;
+        let artistName = document.getElementById('artistName');
+        artistName.innerText = album.artist.name;
+        let year = document.getElementById('year');
+        year.innerText = album.release_date;
+        let numTracks = document.getElementById('numTracks');
+        numTracks.innerText = album.nb_tracks;
+        let totDuration = document.getElementById('totDuration');
+        totDuration.innerText = album.duration;     
 
         for (let i = 0; i < songArray.length; i++){
             let newRow = document.createElement('tr');
